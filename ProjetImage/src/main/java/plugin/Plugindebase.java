c2e13e26210d0ef2a3037ed5dd16635a11fa5e9e
Landrodie Alexandre <alexandre.landrodie@gmail.com>
2018-03-21 11:39:45 +0100

    Add files via upload

--- ProjetImage/src/main/java/plugin/Plugindebase.java
+++ ProjetImage/src/main/java/plugin/Plugindebase.java
@@ -5,54 +5,52 @@ import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import net.imagej.Dataset;
-import net.imagej.DatasetService;
 import net.imagej.ImgPlus;
-import net.imagej.ops.OpService;
 import net.imglib2.RandomAccess;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 
 @Plugin(type = Command.class, menuPath = "Plugins>Projet > PlugindeBase (Not normalized)")
-public class Plugindebase implements Command{
-		@Parameter
-		OpService ops;
-		@Parameter
-		DatasetService dss;
+public class Plugindebase<T extends RealType<T>>  implements Command{
 
-
-		@Parameter
-		Dataset img;
+		@Parameter(persist = false)
+		ImgPlus<T> img;
 
 		@Parameter(type = ItemIO.OUTPUT)
-		ImgPlus<RealType<?>> outImgP;
+		ImgPlus<UnsignedByteType> imageConv;
 
 		@Override
 		public void run() {
-			// Take img dataset as random accessible interval
-			@SuppressWarnings("rawtypes")
-			RandomAccessibleInterval image = img;
 			
-			RandomAccessibleInterval<RealType<?>> result = img;
-			long[] dimension = new long[2];
-			long[] position = new long[2];
-			image.dimensions(dimension);
-			for (int i = 0; i < dimension[0] ;i++) {
-				position[0]=i;
-				for (int j = 0; j < dimension[1];j++) {
-					position[1]=j;
+			long[] dimensions = new long[img.numDimensions()];
+			img.dimensions(dimensions);
+			// Creation of the resulting image with the same size as the input image.
+			imageConv = ImgPlus.wrap(ArrayImgs.unsignedBytes(dimensions));
+			imageConv.setName(img.getName() + "_Mask");
 
+			// Two random cursor to visit all pixels in the input and output images.
+			RandomAccess<T> cursorIn = img.randomAccess();
+			RandomAccess<UnsignedByteType> cursorOut = imageConv.randomAccess();
+			appliquerSeuillage(127, cursorIn,cursorOut,dimensions);
+		}
+		private void appliquerSeuillage(int seuil, RandomAccess<T> cursorIn, RandomAccess<UnsignedByteType> cursorOut,
+				long[] dimensions) {
+			long[] position = new long[3];
+			for (int i = 0; i < dimensions[0] ;i++) {
+				position[0]=i;
+				for (int j = 0; j < dimensions[1];j++) {
+					position[1]=j;
+					cursorIn.setPosition(position);
+					cursorOut.setPosition(position);
+					if(cursorIn.get().getRealDouble()<seuil)
+						cursorOut.get().set(0);
+					else
+						cursorOut.get().set(255);
+							
 				}
 			}
-			Img<RealType<?>> outImg = dss.create(result);
-
-
-			outImgP = new ImgPlus<RealType<?>>(outImg, "img");
-				
 		}
-
 	}
 
 
